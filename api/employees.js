36818d5c5fd6b95f2645eaf82842731311daf390
Your Name <jasonbataillon@gmail.com>
2024-10-09 01:53:53 -0400

    Made some headway with creating error handling middleware.

---
+++ api/employees.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+module.exports = router;
+
+const employees = require('../employees');
+
+router.get('/', (req, res) => {
+  res.json(employees);
+});
+
+router.get('/employees', (req, res) => {
+  res.json(employees);
+});
+
+router.get('/employees/random', (req, res) => {
+  const i = Math.floor(Math.random() * employees.length);
+  res.json(employees[i]);
+});
+
+router.get('/employees/:id', (req, res) => {
+  const { id } = req.params;
+  const employee = employees.find((e) => e.id === +id);
+  if (employee) {
+    res.json(employee);
+  } else {
+    res.status(404).send(`There is no employee with id ${id}.`);
+  }
+});
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello employees!");
+app.get('/', (req, res) => {
+  res.send('Hello employees!');
 });
 
-const employees = require("./employees");
+const employees = require('./employees');
 
-app.get("/employees", (req, res) => {
+app.get('/employees', (req, res) => {
   res.json(employees);
 });
 
-app.get("/employees/random", (req, res) => {
+app.get('/employees/random', (req, res) => {
   const i = Math.floor(Math.random() * employees.length);
   res.json(employees[i]);
 });
 
-app.get("/employees/:id", (req, res) => {
+app.get('/employees/:id', (req, res) => {
   const { id } = req.params;
   const employee = employees.find((e) => e.id === +id);
   if (employee) {
@@ -25,6 +25,9 @@ app.get("/employees/:id", (req, res) => {
   } else {
     res.status(404).send(`There is no employee with id ${id}.`);
   }
+});
+app.use((req, res, next) => {
+  res.status(404).send('Route not found.');
 });
 
 app.listen(PORT, () => {
